8761456f5fbd2c2113816249d90a3a33fd344128
komuro-hiraku <komuro.hiraku@classmethod.jp>
2023-01-06 15:33:56 +0900

    Add Spotless
    - Add Spotless Java Rules

--- src/main/java/jp/classmethod/toys/Main.java
+++ src/main/java/jp/classmethod/toys/Main.java
@@ -1,7 +1,8 @@
+/* Copyright (C) 2023 komuro-hiraku */
 package jp.classmethod.toys;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 }
--- src/test/java/jp/classmethod/toys/interpreter/InterpreterTest.java
+++ src/test/java/jp/classmethod/toys/interpreter/InterpreterTest.java
@@ -1,23 +1,19 @@
+/* Copyright (C) 2023 komuro-hiraku */
 package jp.classmethod.toys.interpreter;
-
-import org.junit.jupiter.api.Test;
 
 import static jp.classmethod.toys.interpreter.Ast.add;
 import static jp.classmethod.toys.interpreter.Ast.integer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 class InterpreterTest {
 
-    private final Interpreter interpreter = new Interpreter();
+  private final Interpreter interpreter = new Interpreter();
 
-    @Test
-    public void test10Plus20ShouldWork() {
-        Ast.Expression e = add(
-                integer(10), integer(20)
-        );
-        assertEquals(
-                30, interpreter.interpret(e)
-        );
-    }
-
+  @Test
+  public void test10Plus20ShouldWork() {
+    Ast.Expression e = add(integer(10), integer(20));
+    assertEquals(30, interpreter.interpret(e));
+  }
 }
